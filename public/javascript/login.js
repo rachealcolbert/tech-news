3aa14bd16500c8000dc3c09459830d65a46ba677
Racheal Colbert <rachealcolbert16@gmail.com>
2021-01-19 02:02:17 -0700

    readme

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,3 +1,29 @@
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
 async function signupFormHandler(event) {
     event.preventDefault();
 
@@ -26,31 +52,7 @@ async function signupFormHandler(event) {
 }
 
 
-async function loginFormHandler(event) {
-    event.preventDefault();
 
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-
-    if (email && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'post',
-            body: JSON.stringify({
-                email,
-                password
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        if (response.ok) {
-            document.location.replace('/');
-        } else {
-            alert(response.statusText);
-        }
-    }
-}
 
 
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
