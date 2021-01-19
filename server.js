a7247362cc74785eb4187844615780d1190d0925
Racheal Colbert <rachealcolbert16@gmail.com>
2021-01-19 00:06:53 -0700

    installed express-session and connect-session-sequelize and added to server.js

--- server.js
+++ server.js
@@ -4,15 +4,29 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+  secret: "hi",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
 app.use(express.json());
 app.use(
-    express.urlencoded({
-        extended: true,
-    })
+  express.urlencoded({
+    extended: true,
+  })
 );
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -25,9 +39,9 @@ app.set("view engine", "handlebars");
 
 // turn on connection to db and server
 sequelize
-    .sync({
-        force: false,
-    })
-    .then(() => {
-        app.listen(PORT, () => console.log("Now listening"));
-    });
+  .sync({
+    force: false,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log("Now listening"));
+  });
