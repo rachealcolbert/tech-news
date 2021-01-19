ee38badf883b9777cc59a2c70f78888cf1753155
Racheal Colbert <rachealcolbert16@gmail.com>
2021-01-18 19:21:13 -0700

    user routes outlined

---
+++ routes/api/user-routes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const {
+    User
+} = require('../../models');
+
+router.get('/', (req, res) => {});
+
+router.get('/:id', (req, res) => {});
+
+router.post('/', (req, res) => {});
+
+router.put('/:id', (req, res) => {});
+
+router.delete('/:id', (req, res) => {});
+
+module.exports = router;
