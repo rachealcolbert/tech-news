8c1a47f3b29732fee709b8b4e9f1572d1d2d4db8
Racheal Colbert <rachealcolbert16@gmail.com>
2021-01-18 19:23:15 -0700

    user.findAll filled out

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,7 +3,14 @@ const {
     User
 } = require('../../models');
 
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+    User.findAll()
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 router.get('/:id', (req, res) => {});
 
